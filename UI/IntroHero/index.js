9fdb4f0a41c481934d00dcf9249e2afc193d14fc
Lee Booth <boothy060509@gmail.com>
2020-12-15 15:51:52 +0000

    user submit done

--- UI/IntroHero/index.js
+++ UI/IntroHero/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import introStyles from './introHero.module.css';
 import {Button} from "../Button";
+import {USER_TURN} from "../Home";
 
-export function IntroHero () {
+export function IntroHero({setPage, setName, name, startGame}) {
    return (
       <div className={introStyles.intro}>
-         <span className={introStyles.logo} />
+         <span className={introStyles.logo}/>
          <p className={introStyles.text}>Welcome to Slam Jam!</p>
          <p className={introStyles.text}>All you need to do is enter a name. Easy!</p>
-         <input type="text" placeholder="ENTER A NAME" className={introStyles.nameInput} />
-         <Button text="GET JAMMIN'" />
+         <input onChange={(event) => setName(event.target.value)} value={name} type="text"
+                placeholder="ENTER A NAME" className={introStyles.nameInput}/>
+         <Button disabled={!name} onClick={() => {
+            setPage(USER_TURN);
+            startGame();
+         }} text="GET JAMMIN!"/>
       </div>
    )
 }
