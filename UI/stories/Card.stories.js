473fe6d67e530818144040c8ef5cc1d9e98651f3
David Hollins <davidhollins06@gmail.com>
2020-12-15 11:53:39 +0000

    Simply styled a card

--- UI/stories/Card.stories.js
+++ UI/stories/Card.stories.js
@@ -14,5 +14,30 @@ const Template = (args) => <Card {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-
+    name: "Strawberry Jam!",
+    description: "Really really tasty",
+    mugShotUrl: "https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+    altText: "Jam on toast",
+    attributes: [
+        {
+            description: "Tastiness",
+            score: 9
+        },
+        {
+            description: "Spreadability",
+            score: 3
+        },
+        {
+            description: "Versatility",
+            score: 5
+        },
+        {
+            description: "Style",
+            score: 2
+        },
+        {
+            description: "Rarity",
+            score: 1
+        }
+    ]
 };
