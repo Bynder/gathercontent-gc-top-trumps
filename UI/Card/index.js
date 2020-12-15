a411aaf12fa80c52cce1cc8ec11105ce926ed477
Alex Smith <altaudiolabs@gmail.com>
2020-12-15 16:19:25 +0000

    Adds styling for selecting attribute of jam

--- UI/Card/index.js
+++ UI/Card/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import cardStyles from './card.module.css';
-import {JamStat} from "../JamStat";
+import React from "react"
+import cardStyles from "./card.module.css"
+import { JamStat } from "../JamStat"
 
-export function Card ({name, description, mugShotUrl, altText, attributes}) {
-   console.log(attributes)
-    return (
-        <div className={cardStyles.card}>
-           <div className={cardStyles.mugShotContainer}>
-               <img alt={altText} src={mugShotUrl} className={cardStyles.mugShot} />
-               <p className={cardStyles.name}>{name}</p>
-           </div>
-            <div className={cardStyles.description}>
-                <p>{description}</p>
-            </div>
+export function Card({
+   name,
+   description,
+   mugShotUrl,
+   altText,
+   attributes,
+   onSelectAttribute,
+   selectedAttribute,
+}) {
+   return (
+      <div className={cardStyles.card}>
+         <div className={cardStyles.mugShotContainer}>
+            <img alt={altText} src={mugShotUrl} className={cardStyles.mugShot} />
+            <p className={cardStyles.name}>{name}</p>
+         </div>
+         <div className={cardStyles.description}>
+            <p>{description}</p>
+         </div>
 
-            <ul className={cardStyles.attributeList}>
-                {attributes.map((attr, index) => (
-                    <JamStat stat={attr.description} value={attr.score} index={index} />
-                ))}
-            </ul>
-        </div>
-    )
+         <ul className={cardStyles.attributeList}>
+            {attributes.map((attr, index) => (
+               <JamStat
+                  stat={attr.description}
+                  value={attr.score}
+                  index={index}
+                  onSelect={() => onSelectAttribute(attr.description)}
+                  selected={selectedAttribute === attr.description}
+                  isAnyAttributeSelected={!!selectedAttribute}
+               />
+            ))}
+         </ul>
+      </div>
+   )
 }
--- UI/UserTurn/index.js
+++ UI/UserTurn/index.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import { map } from "lodash"
 import { Card } from "../Card"
 
 export function UserTurn({ usersTurnCard: { name, cardDescription, ...attributes } }) {
-   const attributesArray = map(attributes, (value, key) => ({ description: key, score: value }))
+   const [selectedAttribute, setSelectedAttribute] = useState(null)
+
+   const parsedAttributes = map(attributes, (value, key) => ({ description: key, score: value }))
 
    return (
       <div>
-         <Card name={name} description={cardDescription} attributes={attributesArray} />
+         <Card
+            name={name}
+            description={cardDescription}
+            attributes={parsedAttributes}
+            selectedAttribute={selectedAttribute}
+            onSelectAttribute={setSelectedAttribute}
+         />
          <button>Slam It!</button>
       </div>
    )
