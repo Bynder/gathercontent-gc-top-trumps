6b81369cec94b3639a32e07a0807dea6214c75a0
David Hollins <davidhollins06@gmail.com>
2020-12-15 10:47:59 +0000

    Adding storybook

---
+++ UI/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function Card () {
+    return (
+        <div>
+            <img></img>
+            <p></p>
+            <ul>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+            </ul>
+        </div>
+    )
+}
