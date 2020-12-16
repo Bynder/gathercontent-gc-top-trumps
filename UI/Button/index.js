55a72cd278bbbedb40b93349ea09c67df4f5fec8
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-12-16 14:00:25 +0000

    Use the Button component, refactoring out positioning

--- UI/Button/index.js
+++ UI/Button/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import buttonStyles from './button.module.css';
 
-export function Button ({onClick, className, text, ...rest}) {
+export function Button ({children, className, ...rest}) {
    return (
-      <button onClick={onClick} className={`${buttonStyles.button} ${className}`} {...rest}>
-         {text}
+      <button className={`${buttonStyles.button} ${className}`} {...rest}>
+         {children}
       </button>
    )
 }
