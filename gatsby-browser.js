a03e4abc4590a91df75bd391060b3b5cf4ae9cf9
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-12-16 20:35:10 +0000

    Create AudioContext and wrap each page with AudioProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import React from 'react';
 import "./src/styles/global.css"
+
+import { AudioProvider } from './src/context/AudioContext';
+
+export const wrapRootElement = ({ element }) => (
+   <AudioProvider>{element}</AudioProvider>
+)
---
+++ src/context/AudioContext.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Howl } from 'howler';
+
+export const AudioContext = React.createContext({ isSoundEnabled: true });
+
+export function AudioProvider({ children }) {
+   const [soundEnabled, setSoundEnabled] = useState(true);
+
+   const background = useRef(new Howl({ src: '/Casual Game Background.mp3', loop: true }));
+   const roundWin = useRef(new Howl({ src: '/Correct Answer.mp3' }));
+   const roundLoose = useRef(new Howl({ src: '/Wrong.mp3' }));
+   const gameWin = useRef(new Howl({ src: '/Win Fanfare (WAV).mp3' }));
+   const gameLoose = useRef(new Howl({ src: '/Game Over Pipe Organ.mp3' }));
+
+   useEffect(() => {
+      if (soundEnabled && !background.current.playing()) {
+         background.current.play()
+      } else {
+         background.current.stop();
+      }
+   }, [soundEnabled])
+
+   return (
+      <AudioContext.Provider value={{
+         playRoundWin: () => soundEnabled && roundWin.current.play(),
+         playRoundLoose: () => soundEnabled && roundLoose.current.play(),
+         playGameWin: () => soundEnabled && gameWin.current.play(),
+         playGameLoose: () => soundEnabled && gameLoose.current.play(),
+         enableSound: () => setSoundEnabled(true),
+         disableSound: () => setSoundEnabled(false),
+         isSoundEnabled: soundEnabled,
+      }}>
+         {children}
+      </AudioContext.Provider>
+   )
+}
