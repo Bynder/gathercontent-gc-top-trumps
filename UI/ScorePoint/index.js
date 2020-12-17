b7019acd43ab59bc1b26e1bb00382c318de9e727
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-12-17 16:07:38 +0000

    ScorePoint can be extended with className

--- UI/ScorePoint/index.js
+++ UI/ScorePoint/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import scorePointStyles from "../ScorePoint/scorePoint.module.css";
 
-export function ScorePoint({ children }) {
-   return <span className={scorePointStyles.scorePointBg}>{children}</span>;
+export function ScorePoint({ children, className }) {
+   return <span className={`${scorePointStyles.scorePointBg} ${className}`}>{children}</span>;
 }
