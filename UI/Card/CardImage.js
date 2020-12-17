05692b2a9b026cc1b6527a572c119d276c3f54c1
Hugh Downer <hugh.downer@gmail.com>
2020-12-17 16:02:25 +0000

    Add width/height to images

--- UI/Card/CardImage.js
+++ UI/Card/CardImage.js
@@ -4,7 +4,7 @@ import cardStyles from "./card.module.css"
 export function CardImage({ name, mugShotUrl, altText }) {
    return (
       <div className={cardStyles.mugShotContainer}>
-         <img alt={altText} src={mugShotUrl} className={cardStyles.mugShot} />
+         <img alt={altText} src={mugShotUrl} className={cardStyles.mugShot} width="355" height="160" />
          <p className={cardStyles.name}>{name}</p>
       </div>
    )
