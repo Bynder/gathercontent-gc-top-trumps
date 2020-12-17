d8af2e1fb3091773b72f295cf8bac8db964a81ec
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2020-12-17 15:18:53 +0000

    Refactor ScorePoint to use SVG icons

--- UI/FinalScore/index.js
+++ UI/FinalScore/index.js
@@ -3,6 +3,7 @@ import finalScoreStyles from './FinalScore.module.css';
 import cardStyles from '../Card/card.module.css';
 import {ScorePoint} from "../ScorePoint";
 import { ReactComponent as LogoSvg } from '../../static/Logo.svg';
+import { Clock, Round, Wins } from '../Icons';
 
 export function FinalScore ({outcome, totalRounds, totalWins, totalTime, score}) {
    return (
@@ -14,19 +15,19 @@ export function FinalScore ({outcome, totalRounds, totalWins, totalTime, score})
             <h1 className={finalScoreStyles.result}>{outcome}</h1>
             <div className={finalScoreStyles.scorePoints}>
                <div className={finalScoreStyles.scorePointRow}>
-                  <ScorePoint scorePointClassName="turnNumber" />
+                  <ScorePoint><Round /></ScorePoint>
                   <span className={finalScoreStyles.scoreTitle}>Total rounds</span>
                   <span className={finalScoreStyles.scoreValue}>{totalRounds}</span>
                </div>
 
                <div className={finalScoreStyles.scorePointRow}>
-                  <ScorePoint scorePointClassName="wins" />
+                  <ScorePoint><Wins /></ScorePoint>
                   <span className={finalScoreStyles.scoreTitle}>Total rounds</span>
                   <span className={finalScoreStyles.scoreValue}>{totalWins}</span>
                </div>
 
                <div className={finalScoreStyles.scorePointRow}>
-                  <ScorePoint scorePointClassName="time" />
+                  <ScorePoint><Clock /></ScorePoint>
                   <span className={finalScoreStyles.scoreTitle}>Total rounds</span>
                   <span className={finalScoreStyles.scoreValue}>{totalTime}</span>
                </div>
