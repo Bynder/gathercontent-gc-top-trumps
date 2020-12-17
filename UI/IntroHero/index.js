98657c88d7e074e9ad87906a0b5f4ed92b6adceb
Lee Booth <boothy060509@gmail.com>
2020-12-17 14:55:52 +0000

    Fixed merge conflict

--- UI/IntroHero/index.js
+++ UI/IntroHero/index.js
@@ -1,40 +1,24 @@
 import React, { useState } from "react"
 import introStyles from "./introHero.module.css"
-import { navigate } from 'gatsby';
 import { Button } from '../Button';
 
-export function IntroHero() {
+export function IntroHero({ onSubmit = console.log }) {
    const [name, setName] = useState("")
 
    return (
-      <form className={introStyles.intro} onSubmit={e => {
-         e.preventDefault()
-         // Implementation of this function is an exercise for the reader.
-         navigate(
-            "/game",
-            {
-               state: { name },
-            }
-         );
-      }}>
+      <form className={introStyles.intro} onSubmit={e => onSubmit(e, { name })}>
          <span className={introStyles.logo} />
          <p className={introStyles.text}>Welcome to Slam Jam!</p>
          <p className={introStyles.text}>All you need to do is enter a name. Easy!</p>
-         <label>
-            Intro: To get started please enter your name.
-            <input
-               onChange={event => setName(event.target.value)}
-               onKeyPress={(event) => { if (event.nativeEvent.key === "Enter") {
-                  navigate("/game", {state: { name }});
-               }}}
-               value={name}
-               type="text"
-               placeholder="ENTER A NAME"
-               className={introStyles.nameInput}
-               name="name"
-            />
-         </label>
-         <Button>
+         <input
+            onChange={event => setName(event.target.value)}
+            value={name}
+            type="text"
+            placeholder="ENTER A NAME"
+            className={introStyles.nameInput}
+            name="name"
+         />
+         <Button className={introStyles.button}>
             GET JAMMIN!
          </Button>
       </form>
