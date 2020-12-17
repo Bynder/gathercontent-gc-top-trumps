32904cd46f25881fdbe6422cee1030735cadacea
David Hollins <davidhollins06@gmail.com>
2020-12-17 16:20:20 +0000

    Removing that darn prettier

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react"
-import { graphql } from "gatsby"
+import React, {useEffect, useState} from "react"
+import {graphql} from "gatsby"
 import { useSpring } from "react-spring"
-import { chunk, map, orderBy, shuffle } from "lodash"
-import { UserTurn } from "../../UI/UserTurn"
 import { Button } from "../../UI/Button"
-import { Result } from "../../UI/Result"
-import { ComputersTurn } from "../../UI/ComputersTurn"
-import { ScoreAside } from "../../UI/ScoreAside"
-import { WaitingCard } from "../../UI/WaitingCard"
-import resultStyles from "../../UI/Result/result.module.css"
-import InfoIcon from "../components/InfoIcon"
+import {chunk, map, orderBy, shuffle} from "lodash"
+import {UserTurn} from "../../UI/UserTurn"
+import {Result} from "../../UI/Result"
+import {ComputersTurn} from "../../UI/ComputersTurn";
+import {ScoreAside} from "../../UI/ScoreAside"
+import {WaitingCard} from "../../UI/WaitingCard";
+import resultStyles from "../../UI/Result/result.module.css";
+import InfoIcon from "../components/InfoIcon";
 
 export const PLAYER_USER = "PLAYER_USER"
 export const PLAYER_COMPUTER = "PLAYER_COMPUTER"
@@ -21,30 +21,31 @@ export const GAME_STATE_WAITING_FOR_COMPUTER = "WAITING_FOR_COMPUTER "
 export const GAME_STATE_COMPUTER_TURN = "COMPUTER_TURN"
 export const GAME_STATE_RESULTS = "STATE_RESULTS"
 
-export default function Game({ data }) {
+export default function Game({data}) {
    const [userProps, userSet] = useSpring(() => ({ opacity: 1 }))
    const [waitingProps, waitingSet] = useSpring(() => ({ opacity: 0, display: "none" }))
 
    const names = [
-      "Barack O - Jar - Ma",
-      "Jamantha Fox",
-      "Halle Berry",
-      "Silence of the Jams",
-      "David Jameron",
-      "Spready Murphy",
-      "Lidney Poitier",
-      "Jam Humphries",
+      'Barack O - Jar - Ma',
+      'Jamantha Fox',
+      'Halle Berry',
+      'Silence of the Jams',
+      'David Jameron',
+      'Spready Murphy',
+      'Lidney Poitier',
+      'Jam Humphries',
    ]
 
    const cards = data.cards.edges.map(card => ({
-      ...card.node,
-      mugshot: card.node.mugshot[0]?.optimised_image_url,
-      rarity: parseInt(card.node.rarity[0]?.label),
-      spreadability: parseInt(card.node.spreadability[0]?.label),
-      versatility: parseInt(card.node.versatility[0]?.label),
-      trendiness: parseInt(card.node.trendiness[0]?.label),
-      tastiness: parseInt(card.node.tastiness[0]?.label),
-   }))
+         ...card.node,
+         mugshot: card.node.mugshot[0]?.optimised_image_url,
+         rarity: parseInt(card.node.rarity[0]?.label),
+         spreadability: parseInt(card.node.spreadability[0]?.label),
+         versatility: parseInt(card.node.versatility[0]?.label),
+         trendiness: parseInt(card.node.trendiness[0]?.label),
+         tastiness: parseInt(card.node.tastiness[0]?.label),
+      })
+   )
 
    const shuffledCards = shuffle(cards)
    const splitCards = chunk(shuffledCards, shuffledCards.length / 2)
@@ -58,15 +59,15 @@ export default function Game({ data }) {
       roundsWon: 0,
       roundWinner: null,
       currentPlayer: PLAYER_USER,
-      selectedAttribute: 0,
+      selectedAttribute: 0
    })
 
-   const incrementTurnCount = () => setAllState({ ...allState, turnCount: allState.turnCount + 1 })
+   const incrementTurnCount = () => setAllState({...allState, turnCount: allState.turnCount + 1})
 
    const usersTurn = (cards = {}) => {
       waitingSet({ opacity: 0, display: "none" })
       userSet({ opacity: 1, display: "block" })
-      setAllState({ ...allState, ...cards, gameState: GAME_STATE_YOUR_TURN })
+      setAllState({...allState, ...cards, gameState: GAME_STATE_YOUR_TURN})
    }
 
    const computersTurn = (cards = {}) => {
@@ -81,25 +82,26 @@ export default function Game({ data }) {
          id,
          ...attributes
       } = allState.computersCards[0]
-      const attributesArray = map(attributes, (value, key) => ({ key: key, value: value }))
+      const attributesArray = map(attributes, (value, key) => ({key: key, value: value}))
       const orderedAttributes = orderBy(attributesArray, ["value"], ["desc"])
 
-      setAllState({ ...allState, ...cards, gameState: GAME_STATE_COMPUTER_TURN })
+      setAllState({...allState, ...cards, gameState: GAME_STATE_COMPUTER_TURN})
 
       setTimeout(() => slamJams(orderedAttributes[0].key), 1500)
    }
 
    const takeTurn = attribute => {
       waitingSet({ opacity: 1, display: "block" })
-      setTimeout(() => slamJams(attribute), 700)
-      userSet({ opacity: 0, display: "none" })
-      setAllState({ ...allState, gameState: GAME_STATE_WAITING_FOR_COMPUTER })
-      setTimeout(() => slamJams(attribute), 700)
+      setTimeout(() => slamJams(attribute), 700);	      userSet({ opacity: 0, display: "none" })
+      setAllState({...allState, gameState: GAME_STATE_WAITING_FOR_COMPUTER})
+      setTimeout(() => slamJams(attribute), 700);
    }
 
    const drawCard = () => {
-      const [usersCard, ...usersRemaining] = allState.usersCards
-      const [computersCard, ...computersRemaining] = allState.usersCards
+
+      const [usersCard, ...usersRemaining] = allState.usersCards;
+      const [computersCard, ...computersRemaining] = allState.usersCards;
+
 
       if (allState.roundWinner === PLAYER_USER) {
          return {
@@ -117,7 +119,7 @@ export default function Game({ data }) {
 
       return {
          usersCards: [...allState.usersCards.splice(1), usersCard],
-         computersCards: [...allState.computersCards.splice(1), computersCard],
+         computersCards: [...allState.computersCards.splice(1), computersCard]
       }
    }
 
@@ -125,30 +127,32 @@ export default function Game({ data }) {
       waitingSet({ opacity: 1, display: "block" })
       userSet({ opacity: 1, display: "block" })
 
-      const [usersCard, ...usersRemaining] = allState.usersCards
-      const [computersCard, ...computersRemaining] = allState.computersCards
+      const [usersCard, ...usersRemaining] = allState.usersCards;
+      const [computersCard, ...computersRemaining] = allState.computersCards;
 
       const hasUserWon = usersCard[attribute] > computersCard[attribute]
       const isDraw = usersCard[attribute] === computersCard[attribute]
 
       if (isDraw) {
+
          setAllState({
             ...allState,
             roundWinner: allState.currentPlayer === PLAYER_USER ? DRAW_PLAYER : DRAW_COMPUTER,
             gameState: GAME_STATE_RESULTS,
-            selectedAttribute: attribute,
+            selectedAttribute: attribute
          })
          return
       }
 
       if (hasUserWon) {
+
          setAllState({
             ...allState,
             roundsWon: allState.roundsWon + 1,
             currentPlayer: PLAYER_USER,
             roundWinner: PLAYER_USER,
             gameState: GAME_STATE_RESULTS,
-            selectedAttribute: attribute,
+            selectedAttribute: attribute
          })
 
          return
@@ -159,21 +163,21 @@ export default function Game({ data }) {
          currentPlayer: PLAYER_COMPUTER,
          roundWinner: PLAYER_COMPUTER,
          gameState: GAME_STATE_RESULTS,
-         selectedAttribute: attribute,
+         selectedAttribute: attribute
       })
 
       return
    }
 
    useEffect(() => {
+
       if (!allState.usersCards.length || !allState.computersCards.length) {
          //Show results
          return
       }
 
-      ;[null, PLAYER_USER, DRAW_PLAYER].includes(allState.roundWinner)
-         ? usersTurn(drawCard())
-         : computersTurn(drawCard())
+      [null, PLAYER_USER, DRAW_PLAYER].includes(allState.roundWinner) ? usersTurn(drawCard()) : computersTurn(drawCard())
+
    }, [allState.turnCount])
 
    return (
@@ -233,38 +237,38 @@ export default function Game({ data }) {
 }
 
 export const pageQuery = graphql`
-   query cardsQuery {
-      cards: allGatherContentItemsByFolderPlayingcards {
-         edges {
-            node {
-               id
-               cardDescription
-               rarity {
-                  label
-               }
-               mugshot {
-                  optimised_image_url
-               }
-               mugshotAltText
-               spreadability {
-                  label
-               }
-               tastiness {
-                  label
-               }
-               trendiness {
-                  label
-               }
-               versatility {
-                  label
-               }
-               name
-               mugshotAltText
-               mugshot {
-                  optimised_image_url
-               }
-            }
-         }
+query cardsQuery {
+  cards: allGatherContentItemsByFolderPlayingcards {
+    edges {
+      node {
+        id
+        cardDescription
+        rarity {
+          label
+        }
+        mugshot {
+          optimised_image_url
+        }
+        mugshotAltText
+        spreadability {
+          label
+        }
+        tastiness {
+          label
+        }
+        trendiness {
+          label
+        }
+        versatility {
+          label
+        }
+        name
+        mugshotAltText
+        mugshot {
+          optimised_image_url
+        }
       }
-   }
+    }
+  }
+}
 `
