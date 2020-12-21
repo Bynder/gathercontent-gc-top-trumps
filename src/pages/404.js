f20d17fb355743ae5bfbe5254a071afca3a0b202
Lee Booth <boothy060509@gmail.com>
2020-12-21 16:03:50 +0000

    started 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import aboutStyles from "../../UI/AboutUs/about.module.css";
+import React from "react";
+
+export default function NotFound() {
+
+   return (
+    <div>
+       <span className={aboutStyles.notFound} />
+    </div>
+   )
+}
