f6f9beb2dda539a753d1f15b16e92a0f13f7f630
Luke Renfrew <luke.renfrew@googlemail.com>
2020-12-22 11:09:57 +0000

    Add image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 import aboutStyles from "../../UI/AboutUs/about.module.css";
 import React from "react";
+import {Button} from "../../UI/Button";
+import {navigate} from "gatsby";
 
 export default function NotFound() {
 
    return (
-    <div>
-       <span className={aboutStyles.notFound} />
-    </div>
+      <div >
+         <span className={aboutStyles.notFound}/>
+         <h1>Jam not found!</h1>
+         <Button onClick={() =>  navigate("/game")}>
+            Get Jammin!
+         </Button>
+      </div>
    )
 }
