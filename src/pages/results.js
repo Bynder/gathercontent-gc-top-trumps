d9796b1b203671948f36991c7b448396fc522665
Alex Smith <altaudiolabs@gmail.com>
2020-12-22 14:48:38 +0000

    Formats results.js

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,26 +1,27 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import introStyles from "../../UI/IntroHero/introHero.module.css"
-import {FinalScore} from "../../UI/FinalScore";
-import {Button} from "../../UI/Button";
-import {navigate} from "gatsby";
+import { FinalScore } from "../../UI/FinalScore"
+import { navigate } from "gatsby"
 
-export default function Results({location}) {
-
+export default function Results({ location }) {
    useEffect(() => {
-      document.addEventListener('keydown', handleEnter, false);
-      return () => document.removeEventListener('keydown', handleEnter)
-   }, []);
+      document.addEventListener("keydown", handleEnter, false)
+      return () => document.removeEventListener("keydown", handleEnter)
+   }, [])
 
-   const handleEnter = (e) => {
-      if (e.key === 'Enter') {
+   const handleEnter = e => {
+      if (e.key === "Enter") {
          navigate("/game")
       }
    }
 
    return (
       <div className={introStyles.container}>
-         <FinalScore won={location.state?.won} totalRounds={location.state?.turnCount}
-                     totalWins={location.state?.roundsWon} timeElapsed={location.state?.timeElapsed}
+         <FinalScore
+            won={location.state?.won}
+            totalRounds={location.state?.turnCount}
+            totalWins={location.state?.roundsWon}
+            timeElapsed={location.state?.timeElapsed}
          />
       </div>
    )
